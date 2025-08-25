013ffee8219d1dd89bb7d5588ee06138d2bca144
Gilson de Jesus <gilsonmdj@gmail.com>
2025-08-25 07:02:09 -0300

    feat: add product list and product item componets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,45 @@
 import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import Image from "next/image";
+import { db } from "@/db";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
+  console.log(products);
+
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-01.png"
+            alt="Leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
